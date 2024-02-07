f712cdd08fa96178c22d5061445169a8af0c6ead
nastya <anastasia.zelenenkaya@aisa.ru>
2024-02-07 16:16:51 +0300

    wip connect media

--- App.tsx
+++ App.tsx
@@ -3,9 +3,18 @@ import {StyleSheet, View} from "react-native";
 import {config} from "@gluestack-ui/config";
 import {Main} from "./screens/main/Main";
 import "./i18n.config";
+import * as MediaLibrary from 'expo-media-library';
+
 
 export default function App() {
-    return (
+  const linkMedia = async () => {
+    const {assets} = await MediaLibrary.getAssetsAsync({first: 5})
+    console.log('MediaLibrary1', assets)
+  }
+
+  linkMedia()
+
+  return (
         <GluestackUIProvider config={config}>
             <View style={styles.container}>
                 <Main/>
